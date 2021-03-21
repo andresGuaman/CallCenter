93674e3e28d8d708be784766092d5b28ab8c3d22
kevin-a1 <kevin.llivichuzhca.est@gmail.com>
2021-03-21 11:26:27 -0500

    Configuración de la cabezera para la conexión con angular

--- modulos/app/__init__.py
+++ modulos/app/__init__.py
@@ -3,6 +3,7 @@ import json
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 import datetime
+from flask_cors import CORS
 
 class JSONEncoder(json.JSONEncoder):
     def default(self, o):
@@ -10,12 +11,14 @@ class JSONEncoder(json.JSONEncoder):
             return str(o)
         if isinstance(o,datetime.datetime):
             return str(o)
-        return json.JSONEncoder.default(self,o)        
+        return json.JSONEncoder.default(self,o)
 
 
-app = Flask(__name__)  
+app = Flask(__name__)
+CORS(app)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+
 app.config['MONGO_URI']= 'mongodb+srv://agmr:123@callcenter.am91v.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 app.json_encoder = JSONEncoder
 mongo = PyMongo(app)
-
-
