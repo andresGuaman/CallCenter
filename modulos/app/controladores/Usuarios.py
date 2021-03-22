466376c1bce043e97f16014b4c0be802b373077d
Andrés <freddy.sapatanga.est@tecazuay.edu.ec>
2021-03-22 09:28:53 -0500

    Update Usuario

--- modulos/app/controladores/Usuarios.py
+++ modulos/app/controladores/Usuarios.py
@@ -12,3 +12,24 @@ def inicio():
         if data == None:
             data =[]
         return jsonify({'transacción':True, 'data':lista})
+
+@app.route('/usuario/crear', methods = ['POST'])
+def create():
+
+    if request.method == 'POST':
+        
+        data = request.get_json()
+        save = mongo.db.usuarios.insert_one(data)
+
+        print (data)
+        
+        if valicacion_ususario(data):
+            return jsonify({'transaccion':True, 'message':'User created successfully' ,'data':list(data)})
+        else:
+            return jsonify({'transaccion':False, 'message':'Datos Incorrectos' ,'data':list(data)})
+
+#Validaciones
+
+def valicacion_ususario(data):
+
+    return True
