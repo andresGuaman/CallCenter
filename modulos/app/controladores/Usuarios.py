a456f008968039109835b50a9cbcdf62d8c06577
Andrés <freddy.sapatanga.est@tecazuay.edu.ec>
2021-03-23 21:36:12 -0500

    Valicación registro usuario

--- modulos/app/controladores/Usuarios.py
+++ modulos/app/controladores/Usuarios.py
@@ -2,7 +2,10 @@ from flask import request, jsonify
 from modulos.app import app, mongo
 import os
 
+#VARIABLES
+
 ROOT_PATCH = os.environ.get('ROOT_PATCH')
+DOMINIO_CORREO_ISTA = '@tecazuay.edu.ec'
 
 @app.route('/usuario/lista', methods = ['GET'])
 def inicio():
@@ -19,17 +22,70 @@ def create():
     if request.method == 'POST':
 
         data = request.get_json()
-        save = mongo.db.usuarios.insert_one(data)
+        response = validar_usuario(data)
 
-        print (data)
-
-        if valicacion_ususario(data):
-            return jsonify({'transaccion':True, 'message':'User created successfully' ,'data':list(data)})
+        if response[0]:
+            save = mongo.db.usuarios.insert_one(data)
+            return jsonify({'transaccion':True, 'message':response[1] ,'data':list(data)})
         else:
-            return jsonify({'transaccion':False, 'message':'Datos Incorrectos' ,'data':list(data)})
+            return jsonify({'transaccion':False, 'message':response[1] ,'data':list(data)})
 
 #Validaciones
 
-def valicacion_ususario(data):
+def validar_usuario(data) -> tuple:
 
-    return True
+    if validar_email(data.get('correo')) == False:
+        return (False, 'Correo incorrecto')
+
+    if validar_nombres([data.get('nombre'), data.get('apellido')]):
+        return (False, 'Nombre o apellido incorreto')
+
+    if validar_password(data.get('password')):
+        return (False, 'Contraseña incorrecta')
+
+    return (True, 'Datos correctos')
+
+def validar_nombres(nombres: list) -> bool:
+
+    if list[0] and list[1]:
+
+        for n in nombres:
+            if n.strip() and n.isalpha() and n.count(' ') < 1:
+                response = True
+            else:
+                response = False
+
+        return response
+    else:
+        return False
+
+def validar_email(correo: str) -> bool:
+
+    if correo:
+        if correo.strip() and len(correo) >= 17 and len(correo) < 75 and correo.endswith(DOMINIO_CORREO_ISTA) and correo.count('@') == 1:
+
+            try:
+                start_email = correo.split('@')[0]
+
+                if start_email.replace('.','').isalpha():
+                    return True
+                else:
+                    return False
+
+            except Exception:
+                return False
+
+        else:
+            return False
+    else:
+        return False
+
+def validar_password(password: str) -> bool:
+
+    if password:
+        if password.strip() and len(password) > 8 and len(password) < 50:
+            return True
+        else:
+            return False
+    else:
+        return False
