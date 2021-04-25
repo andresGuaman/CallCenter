2ec505160204d39d96c1b18148ab660ef801e055
kellyFarfan <kelly.farfan.est@tecazuay.edu.ec>
2021-04-24 19:46:00 -0500

    Crud Usuario

--- modulos/app/controladores/Usuarios.py
+++ modulos/app/controladores/Usuarios.py
@@ -1,5 +1,6 @@
 from modulos.app import app, mongo
 from flask import request, jsonify, session
+from bson.objectid import ObjectId
 
 import os
 
@@ -24,45 +25,76 @@ def login_pass(email, passw):
         if data:
             return jsonify({'transacción':True,'message':'Login Correcto', 'data':passw})
         else:
-            return jsonify({'transacción':False,'message':'Contraseña incorrecta', 'data':passw})   
+            return jsonify({'transacción':False,'message':'Contraseña incorrecta', 'data':passw})
+
+@app.route('/usuario/eliminar', methods=['POST'])
+def delete_usuario():
+    if request.method == 'POST':
+        data = request.get_json()
+        if data != None:
+            data['_id'] = ObjectId(data.get('_id'))
+            mongo.db.usuarios.delete_one(data)
+            return jsonify({"transaccion":True, "mensaje":"Usuario eliminado correctamente", 'data':data})
+
+
+@app.route('/usuario/list', methods = ['GET'])
+def listar_user():
+    
+    if request.method == 'GET':
+
+        data = mongo.db.usuarios.find({})
+    
+        if data and data != None:
+            return jsonify({"transaccion":True, 'mensaje':'Transacción exitosa','data':list(data)})
+        else:
+            return jsonify({"transaccion":False, 'mensaje':'Error de transacción', "data":[]})
+
+@app.route("/usuario/update/<string:email>",  methods=['POST'])
+def update_one(email):
+    
+    if request.method == 'POST':
+        data = request.get_json()
+        result = mongo.db.usuarios.update({'correo': email}, {'correo': email, 'nombre':data.get('nombre'),
+        'apellido': data.get('apellido'), 'password':data.get('password')})
+        if result: 
+            return jsonify({'transaccion':True, 'message':"Modificacion Exitosa",'data':list(data)})
+        else:
+            return jsonify({'transaccion':False, 'message':"Error al editar" ,'data':list(data)})
+
+
 
 @app.route('/usuario/crear', methods = ['POST'])
 def create():
-
     if request.method == 'POST':
-
         data = request.get_json()
         response = validar_usuario(data)
-
         if response[0] == True:
             save = mongo.db.usuarios.insert_one(data)
             return jsonify({'transaccion':True, 'message':response[1] ,'data':list(data)})
         else:
             return jsonify({'transaccion':False, 'message':response[1] ,'data':list(data)})
 
+
 #Validaciones
 
 def validar_usuario(data) -> tuple:
-
     if data.get('correo'):
         if validar_email(data.get('correo')) == False:
             return (False, 'Correo incorrecto')
     else:
         return (False, 'Correo Vacio')
-
     if data.get('nombre') and data.get('apellido'):
         if validar_nombres([data.get('nombre'), data.get('apellido')]) == False:
             return (False, 'Nombres incorretos')
     else:
         return (False, 'Nombres vacíos')
-
     if data.get('password'):
         if validar_password(data.get('password') == False):
             return (False, 'Contraseña incorrecta')
     else:
         return (False, 'Contraseña vacío')
-
     return (True, 'Datos correctos')
+
 
 def validar_nombres(nombres: list) -> bool:
     if nombres[0] and nombres[1]:
@@ -78,21 +110,16 @@ def validar_nombres(nombres: list) -> bool:
         return False
 
 def validar_email(correo: str) -> bool:
-
     if correo:
         if correo.strip() and len(correo) >= 17 and len(correo) < 75 and correo.endswith(DOMINIO_CORREO_ISTA) and correo.count('@') == 1:
-
             try:
                 start_email = correo.split('@')[0]
-
                 if start_email.replace('.','').isalpha():
                     return True
                 else:
                     return False
-
             except Exception:
                 return False
-
         else:
             return False
     else:
